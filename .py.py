3b00000940dd50c50401e2ef1a9bc94c3fad104b
Jaques31 <77124098+Jaques31@users.noreply.github.com>
2021-01-07 19:04:24 -0300

    Add files via upload

---
+++ .py.py
@@ -0,0 +1,17 @@
+
+
+primeiro_numero = float(input('Digite o primeiro número: ' ))
+segundo_numero = float(input('Digite o segundo número: ' ))
+
+
+soma = primeiro_numero + segundo_numero
+subtracao = primeiro_numero - segundo_numero
+multiplicacao = primeiro_numero * segundo_numero
+divisao = primeiro_numero / segundo_numero
+
+
+
+print('A soma é', soma)
+print('A subtração é' , subtracao)
+print('A multiplicação é',multiplicacao)
+print('A divisão é', divisao)
